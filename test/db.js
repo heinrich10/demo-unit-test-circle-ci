0bb114312b14e0aaa6c3ea054617a662358ee8d9
Heinrich Chan <guiltyrage10@gmail.com>
2018-08-25 09:38:32 +0800

    refactored the code based on the new eslint rules

--- test/db.js
+++ test/db.js
@@ -1,42 +1,44 @@
 const chai = require('chai');
-const expect = chai.expect;
+const { expect } = chai;
 const db = require('../lib/db');
 const sinon = require('sinon');
 
-describe('Temperature Scrapper', function () {
-	describe('Test db.set', function () {
-		it('Should save to the DB', function () {
-			let spy = sinon.stub();
-			let redis = {
+describe('Temperature Scrapper', () => {
+	describe('Test db.set', () => {
+		it('Should save to the DB', () => {
+			const spy = sinon.stub();
+			const redis = {
 				set: spy
 			};
-			let DB = new db(redis);
+			const DB = new db(redis);
 			DB.set('test', 'value');
 			expect(spy.calledOnce).to.be.true;
 		});
 	});
-	describe('Test db.get', function () {
-		it('Should get from the DB', function (done) {
-			let spy = sinon.stub();
-			let redis = {
+	describe('Test db.get', () => {
+		it('Should get from the DB', (done) => {
+			const spy = sinon.stub();
+			const redis = {
 				get: spy
 			};
-			let DB = new db(redis);
-			DB.get('test', function (err, result) {
+			const DB = new db(redis);
+			const res = 123;
+			DB.get('test', (err, result) => {
 				expect(err).to.be.null;
-				expect(result).to.be.equal(123);
+				expect(result).to.be.equal(res);
 				done();
 			});
-			spy.callArgWith(1, null, 123);
+			spy.callArgWith(1, null, res);
 		});
-		it('Should return an error when something goes wrong', function (done) {
-			let spy = sinon.stub();
-			let redis = {
+		it('Should return an error when something goes wrong', (done) => {
+			const spy = sinon.stub();
+			const redis = {
 				get: spy
 			};
-			let DB = new db(redis);
-			DB.get('test', function (err, result) {
+			const DB = new db(redis);
+			DB.get('test', (err, result) => {
 				expect(err).to.be.instanceof(Error);
+				expect(result).to.be.null;
 				done();
 			});
 			spy.callArgWith(1, new Error(), null);
--- test/scraper.js
+++ test/scraper.js
@@ -1,39 +1,41 @@
 const request = require('request');
 const chai = require('chai');
-const expect = chai.expect;
+const { expect } = chai;
 const sinon = require('sinon');
 const scrape = require('../lib/scraper');
 
 
-describe('Temperature Scrapper', function () {
+describe('Temperature Scrapper', () => {
 	let req;
-	afterEach(function () {
+	afterEach(() => {
 		req.restore();
 	});
-	it('Should return the temparature', function (done) {
+	it('Should return the temparature', (done) => {
 		req = sinon.stub(request, 'get');
 
-		scrape(function (err, temp) {
+		scrape((err, temp) => {
 			expect(err).to.be.null;
 			expect(temp).to.be.equal(0);
 			done();
 		});
 		req.yield(null, {statusCode: 200}, '{"main":{"temp": 273}}');
 	});
-	it('Should return an error when status code is greater than 400', function (done) {
+	it('Should return an error when status code is greater than 400', (done) => {
 		req = sinon.stub(request, 'get');
 
-		scrape(function (err, temp) {
+		scrape((err, temp) => {
 			expect(err).to.be.instanceof(Error);
+			expect(temp).to.be.undefined;
 			done();
 		});
 		req.yield(null, {statusCode: 400}, '{"main":{"temp": 273}}');
 	});
-	it('Should return an error when something goes wrong', function (done) {
+	it('Should return an error when something goes wrong', (done) => {
 		req = sinon.stub(request, 'get');
 
-		scrape(function (err, temp) {
+		scrape((err, temp) => {
 			expect(err).to.be.instanceof(Error);
+			expect(temp).to.be.undefined;
 			done();
 		});
 		req.yield(new Error());
